5d88e4aadbaec5bbd1fe8beb5a5fa102dc269eb7
Mohmmed-Olimat <73778028+Mohmmed-Olimat@users.noreply.github.com>
2021-01-16 10:16:20 +0200

    Add Admin CRUD

--- OES/Admin/edit_admin.php
+++ OES/Admin/edit_admin.php
@@ -1,78 +1,82 @@
+<?php
+ 
+   include('includes/adminClass.php');
+   $x = new Admin();
+   $id =$_GET['id'];
+
+    if(isset($_POST['submit'])){
+      
+        
+    
+    $x->admin_email = $_POST['admin_email'];
+    $x->password    = $_POST['admin_password'];
+    $x->admin_name  = $_POST['admin_fullname'];
+    $q=$x->update($id);
+    
+    if($q){
+        header("location:manageadmin.php");
+        
+    }
+}       
+        $data=$x->readById($id);
+        $adminSet=$data[0];
+       
+
+
+
+
+?>
  <?php include("includes/admin_header.php");?>
 
 
 
 
        <!-- Body -->
-   
 
-                
-                        <div class="sparkline12-list">
-                            
-                                <div class="main-sparkline12 text-center">
-                                    <h2>Update Admin</h2>
-                                </div>
-                         
-                                   <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                            
-                                                <form action="#" class="pl-5">
-                                                
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
-                                                                Full Name
-                                                                <input type="text" class="form-control" /> </div>
-                                                        </div>
-                                                    </div>
-                                               
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
-                                                                Email
-                                                                <input type="email" class="form-control" />
-                                                         </div>
-                                                        </div>
-                                                    </div>
+       <div class="sparkline12-list">
+        <div class="main-sparkline12 text-center">
+            <h2>Edit Admin</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <form action="" class="pl-5" method="post">                                              
+                    <div class="form-group-inner">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
+                                Full Name
+                                <input type="text" class="form-control" name="admin_fullname" value="<?php 
+                                echo $adminSet['admin_name'];?>"/>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="form-group-inner">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
+                                Email
+                                <input type="email" class="form-control" name="admin_email" value="<?php
+                                 echo $adminSet['admin_email']; ?>"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group-inner">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
+                             Password
+                             <input type="password" class="form-control" name="admin_password" value="<?php
+                              echo $adminSet['password']; ?>"/>
+                         </div>  
+                     </div>
+                 </div>    
+                 <div>      
 
+                   <button class="btn btn-lg btn-info col-lg-12" type="submit" name="submit">
+                    Save
+                </button>
 
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
-                                                               Password
-                                                                <input type="password" class="form-control" />
-                                                      </div>  
-                                                  </div>
-                                                    </div>
-                                                  
-                                                 
-                                                 
-                                              <div >      
-                                                     
-                                         <button class="btn btn-lg btn-info col-lg-12" type="submit">
-                                            Save
-                                         </button>
-                                           
-                                                   </div>    
+            </div>    
+        </form>
+    </div>
+</div>
+</div>
 
-
-                                                </form>
-                                           
-                                        </div>
-                                    </div>
-
-
-
-
-                                    
-                                
-        <!-- Body End -->
-
-
-
-  <?php include("includes/admin_footer.php");?>
-
-
-
-       
--- OES/Admin/manageadmin.php
+++ OES/Admin/manageadmin.php
@@ -1,120 +1,135 @@
+ <?php
+    
+   include('includes/adminClass.php');
+   $x = new Admin();
+
+    if(isset($_POST['submit'])){
+      
+        
+    
+    $x->admin_email = $_POST['admin_email'];
+    $x->password    = $_POST['admin_password'];
+    $x->admin_name  = $_POST['admin_fullname'];
+    $q=$x->create();
+    
+    if($q){
+        header("location:manageadmin.php");
+        
+    }
+}
+ ?>
+
+
+
+
+
  <?php include("includes/admin_header.php");?>
 
 
 
 
        <!-- Body -->
-    
-   
 
-                
-                        <div class="sparkline12-list">
-                            
-                                <div class="main-sparkline12 text-center">
-                                    <h2>Create Admin</h2>
-                                </div>
-                         
-                                   <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                            
-                                                <form action="#" class="pl-5">
-                                                
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
-                                                                Full Name
-                                                                <input type="text" class="form-control" /> </div>
-                                                        </div>
-                                                    </div>
-                                               
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
-                                                                Email
-                                                                <input type="email" class="form-control" />
-                                                         </div>
-                                                        </div>
-                                                    </div>
-
-
-
-                                                    <div class="form-group-inner">
-                                                        <div class="row">
-                                                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
-                                                               Password
-                                                                <input type="password" class="form-control" />
-                                                      </div>  
-                                                  </div>
-                                                    </div>
-                                                  
-                                                 
-                                                 
-                                              <div >      
-                                                     
-                                         <button class="btn btn-lg btn-info col-lg-12" type="submit">
-                                            Save
-                                         </button>
-                                           
-                                                   </div>    
-
-
-                                                </form>
-                                           
-                                        </div>
-                                    </div>
-                            
-                     
-     
-
-
-                                    </div>
-
-
-
-
-                                    
-                                <div class="sparkline12-list mg-tb-30">
-                            
-                                
-                         
-                                   <div class="row">
-                                         <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12">
-                        <div class="sparkline8-list">
-                           
-                            <div class="sparkline8-graph">
-                                <div class="static-table-list">
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Full_Name</th>
-                                                <th>Email</th>
-                                                <th>Password</th>
-                                                <th>Update</th>
-                                                <th>Delete</th>
-                                            </tr>
-                                        </thead>
-                                                                                <tbody>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
-                                        <td><a href="edit_admin.php" class="btn btn-primary">Edit</a></td>
-                                        <td><a href="delete_admin.php" class="btn btn-danger">Delete</a></td>
-                                        </tbody>
-                                    </table>
-                                </div>
+       <div class="sparkline12-list">
+        <div class="main-sparkline12 text-center">
+            <h2>Create Admin</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <form action="" class="pl-5" method="post">                                              
+                    <div class="form-group-inner">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
+                                Full Name
+                                <input type="text" class="form-control" name="admin_fullname" />
                             </div>
                         </div>
                     </div>
-                                    </div>
-                            
-                     
-     
+
+                    <div class="form-group-inner">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
+                                Email
+                                <input type="email" class="form-control" name="admin_email"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group-inner">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-3 col-sm-3 col-xs-12">
+                             Password
+                             <input type="password" class="form-control" name="admin_password" />
+                         </div>  
+                     </div>
+                 </div>    
+                 <div>      
+
+                   <button class="btn btn-lg btn-info col-lg-12" type="submit" name="submit">
+                    Save
+                </button>
+
+            </div>    
+        </form>
+    </div>
+</div>
+</div>
 
 
-                                    
-                                    </div>
+
+
+
+<div class="sparkline12-list mg-tb-30">
+
+
+
+ <div class="row">
+   <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12">
+    <div class="sparkline8-list">
+
+        <div class="sparkline8-graph">
+            <div class="static-table-list">
+                <table class="table">
+                    <thead class="" style="background-color: gray;">
+                        <tr>
+                            <th>ID</th>
+                            <th>Email</th>
+                            <th>Name</th>
+                            <th>Password</th>
+                            <th>Edit</th>
+                           <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                          if($data=$x->readAll()){
+                         
+                                        foreach ($data as $key => $value) {
+                                            foreach ($value as $k => $v) {$row[$k]=$v;}
+                                                echo "<tr>";
+                                                echo "<td>{$row['admin_id']}</td>";
+                                                echo "<td>{$row['admin_email']}</td>";
+                                                echo "<td>{$row['admin_name']}</td>";
+                                                echo "<td>{$row['password']}</td>";
+
+                                                echo "<td><a href='edit_admin.php?id={$row['admin_id']}' class='btn btn-warning'>Edit</a></td>";
+                                                echo "<td><a href='delete_admin.php?id={$row['admin_id']}' class='btn btn-danger'>Delete</a></td>";
+                                                echo "</tr>";
+                                            
+                                            
+                                        }
+                                           
+
+                                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
        
         <!-- Body End -->
 
